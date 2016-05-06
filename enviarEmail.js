3cbcdfbeec4e4123d9e78dcb4e4cff2caf147044
Theus Ribeiro <matheusohn@gmail.com>
2016-05-06 16:05:22 -0300

    Classe em javaScript para envio de email

---
+++ enviarEmail.js
@@ -0,0 +1,11 @@
+function onFormSubmit(e) {
+    var toAddress = "destino@servidor.com.br";
+    var timestamp = e.values[0];
+    var answer = e.values[1];
+    var subject = "New answer";
+    var emailBody = "Corpo do Email, blahblah..." + "\n\n" +
+                    "A resposta é: " + answer + "\n\n" +
+                    "Hora de envio: " + timestamp;
+    MailApp.sendEmail(toAddress, subject, emailBody);
+}
+
